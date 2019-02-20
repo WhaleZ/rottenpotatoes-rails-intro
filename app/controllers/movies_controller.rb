22dc803a66dd91ad58b51bd5513b9e1b70d297d6
whalez <whalez@tamu.edu>
2019-02-20 16:41:10 -0600

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,10 +8,14 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+
   end
 
   def index
-    @movies = Movie.all
+    #  @movies = Movie.all
+    @all_ratings = Movie.ratings
+    @sort = params[:sort]
+    @movies = Movie.all.order(@sort)
   end
 
   def new
